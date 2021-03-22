37212c2715c6a425fca5b3eedda198331708285c
Ramakrishnan Kandasamy <rmkanda@thoughtworks.com>
2021-03-22 09:42:00 +0530

    [Ramki] Add past editions

--- src/edition/list.jsx
+++ src/edition/list.jsx
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 import { Banner } from "../home/banner/index.jsx";
+import { EditionCard } from "./card.jsx";
 
 import "./index.scss";
 
@@ -13,9 +15,32 @@ export class PastEditions extends Component {
   render() {
     const event = this.props.config;
 
+    const renderEditions = () => {
+      const editions = event.editions;
+
+      const pastEditions = editions.filter(
+        (edition) => edition.endTime < new Date().valueOf()
+      );
+
+      return (
+        <div id="editions" className="recentEditions">
+          <span className="banner">Recent Editions</span>
+          <Link className="view-all" to="./list">
+            View All
+          </Link>
+          <div className="editions">
+            {pastEditions.map((edition) => {
+              return <EditionCard key={edition.id} edition={edition} />;
+            })}
+          </div>
+        </div>
+      );
+    };
+
     return (
       <div id="event-home" className="event-home">
         <Banner event={event}></Banner>
+        {renderEditions()}
       </div>
     );
   }
